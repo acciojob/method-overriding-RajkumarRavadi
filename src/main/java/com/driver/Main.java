816017715526b71d040e3763c57d1f7c9f288654
RajkumarRavadi <rajk142567@gmail.com>
2024-07-17 10:03:20 +0530

    try3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,25 +1,8 @@
 package com.driver;
 
 public class Main {
-    public static void main(String [] args){
-        // Create an object of class B
+    public static void main(String[] args) {
         B objb = new B();
-
-        // Call method meth of class A with it
         System.out.println(objb.meth());
-    }
-}
-
-class A {
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
-
-class B extends A {
-    // Override method meth of class A
-    @Override
-    public String meth(){
-        return "Method is overridden in Extended class B";
     }
 }

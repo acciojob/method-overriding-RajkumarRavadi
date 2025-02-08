a4c65a4a855549ce902b087ab7a76ee7711c2aaa
RajkumarRavadi <rajk142567@gmail.com>
2025-02-08 16:35:57 +0530

    printing remoed;

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,9 +3,9 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         A obja = new B();
-        System.out.println(obja.meth());
+        obja.meth();
 
         B objb = new B();
-        System.out.println(objb.meth());
+        objb.meth();
     }
 }

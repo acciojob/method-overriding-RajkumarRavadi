32ab3dcbdf7ae41f52258ac3967da6e5fea8d4e1
RajkumarRavadi <rajk142567@gmail.com>
2025-02-08 17:22:22 +0530

    deleted a and b classes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,8 +4,25 @@ public class Main {
     public static void main(String[] args) {
         A obja = new B();
         obja.meth();
+        System.out.println(obja.meth());
 
         B objb = new B();
         objb.meth();
+        System.out.println(objb.meth());
+    }
+
+    static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    static class B extends A {
+        @Override
+        public String meth() {
+            return "Method is overridden in Extedend class B";
+        }
     }
 }
+
+

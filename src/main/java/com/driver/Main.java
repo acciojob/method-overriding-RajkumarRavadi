550211dc6d95062a0dabef45543a545eadf9d31e
RajkumarRavadi <rajk142567@gmail.com>
2025-02-08 17:24:33 +0530

    ade class a and b public

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -11,13 +11,13 @@ public class Main {
         System.out.println(objb.meth());
     }
 
-    static class A {
+    public static class A {
         public String meth() {
             return "Invoking method from class A";
         }
     }
 
-    static class B extends A {
+    public static class B extends A {
         @Override
         public String meth() {
             return "Method is overridden in Extedend class B";
